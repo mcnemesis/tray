616357b32b51e5836c2f2b2e4a221d4cd1194454
Ow. Nemesis Fixx <joewillrich@gmail.com>
2020-10-27 21:59:15 +0300

    Update in-App app name to Meta Tray

--- app/src/main/java/com/nuchwezi/tray/MainActivity.java
+++ app/src/main/java/com/nuchwezi/tray/MainActivity.java
@@ -70,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setTitle(R.string.in_app_name);
+
         // for the animated background...
         AnimationDrawable animDrawable = (AnimationDrawable) findViewById(R.id.rootLayout).getBackground();
         animDrawable.setEnterFadeDuration(10);

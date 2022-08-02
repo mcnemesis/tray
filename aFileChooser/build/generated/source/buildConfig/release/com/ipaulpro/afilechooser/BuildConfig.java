5412e67725d202776fcb93fec89fea6b8f88771d
Joseph Willrich Lutalo <joewillrich@gmail.com>
2022-08-02 11:03:08 +0300

    necessary file access facility

---
+++ aFileChooser/build/generated/source/buildConfig/release/com/ipaulpro/afilechooser/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ipaulpro.afilechooser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.ipaulpro.afilechooser";
+  public static final String BUILD_TYPE = "release";
+}
